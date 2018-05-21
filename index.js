a42489fe6cf78675e864eecc974a1270429db2c1
Maxwell Benton <maxwellbenton@gmail.com>
2018-05-21 14:23:25 -0400

    Update index.js
    removed solution

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
 // Write your code in this file!
-const currentUser = 'Grace Hopper';
 
-const welcomeMessage = `Welcome to Klossybook, ${currentUser}!`;
-
-const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-
-const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
