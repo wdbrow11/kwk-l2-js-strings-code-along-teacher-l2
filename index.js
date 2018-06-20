0897aec7a10bcc17f7f881571cbf7fadda92059e
wdbrow11 <will.brown.hc@gmail.com>
2018-06-20 03:13:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // Write your code in this file!
 let currentUser = 'Will';
-let welcomeMessage = ('Welcome to Klossybook, ' , currentUser , "!");
+let welcomeMessage = ("Welcome to Klossybook, " + currentUser + "!");
