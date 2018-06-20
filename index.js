8a63ba5660f7170b9e2238b27f9e76d10be6e9d5
wdbrow11 <will.brown.hc@gmail.com>
2018-06-20 03:19:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 // Write your code in this file!
-
+let currentUser = 'Will';
+let welcomeMessage = ("Welcome to Klossybook, " + currentUser + "!");
+let excitedWelcomeMessage = welcomeMessage.toUpperCase()
+let shortGreeting = ("Welcome, " + currentUser[0] + "!")
