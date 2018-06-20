9291d2fe9598be5e2e57819d59e810adf2ef8ace
wdbrow11 <will.brown.hc@gmail.com>
2018-06-20 03:15:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
 let currentUser = 'Will';
 let welcomeMessage = ("Welcome to Klossybook, " + currentUser + "!");
-let excitedWelcomeMessage = welcomeMessage.upper()
+let excitedWelcomeMessage = upper.welcomeMessage()
